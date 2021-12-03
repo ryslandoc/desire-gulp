07602caaf25bd9d466291e0e2afa8d6fbaeb9d22
Ruslan Stetsenko <ruslandoc00@gmail.com>
2021-12-03 18:15:47 +0200

    adding a finished website to git without adaptive

---
+++ app/js/main.js
@@ -0,0 +1,35 @@
+$(function () {
+
+    $('.header__btn').on('click', function () {
+        $('.rightside-menu').removeClass('rightside-menu__close')
+    })
+    $('.rightside-menu__close').on('click', function () {
+        $('.rightside-menu').addClass('rightside-menu__close')
+    })
+
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true
+    });
+
+    $('.contact-slider').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 10,
+        slidesToScroll: 10,
+    });
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow right"></button>',
+    });
+
+
+    var mixer = mixitup('.gallery__inner', {
+        load: {
+            filter: '.living'
+        }
+    });
+})
